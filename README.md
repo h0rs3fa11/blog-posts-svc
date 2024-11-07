daade7255d8c374700d8f87cd7e31f7c74251f55
Q.Ma <corrie.ma@protonmail.com>
2024-11-07 20:55:42 +0100

    add register form and route

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 A full-featured blog web app using Flask Framework
+
+```
+pip install -r requirements.txt
+```
--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '16dce8fe2af2c6de77f9b493d3bc5cdb'
 
 posts = [
     {
@@ -18,14 +21,26 @@ posts = [
 
 ]
 
+
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
 
+
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
